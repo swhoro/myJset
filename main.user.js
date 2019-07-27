bf3a72eefe73023ca6b9d9424a146c0c4027998a
swhoro <1003077133@qq.com>
2019-07-27 18:37:51 +0800

    now can replace all deleted links

--- main.user.js
+++ main.user.js
@@ -1,19 +1,24 @@
 // ==UserScript==
-// @name         steambaidupan_unlock
+// @name         steamDuPanUnlock
 // @version      0.6
 // @description  万恶的steam封锁了百度盘链接，此脚本可用来解封
 // @author       Aiden
 // @match        https://steamcommunity.com/*
+// @match        https://store.steampowered.com/*			
 // @grant        none
 // @updateURL	 https://raw.githubusercontent.com/swhoro/steambaidupan_unlock/master/main.user.js
 // ==/UserScript==
 
 (function () {
-    var uglyWhiteWords = document.querySelector(".bb_removedlink");
-    uglyWhiteWords.parentElement.removeChild(uglyWhiteWords);
+    var uglyWhiteWords = document.getElementsByClassName("bb_removedlink");
+    for (let i = 0; i < uglyWhiteWords.length; i++) {
+        uglyWhiteWords[i].parentElement.removeChild(uglyWhiteWords[i]);
+    }
 
-    var fixedALabel = document.querySelector(".collapsed_link");
-    fixedALabel.style.display = "inline";
-    fixedALabel.href = fixedALabel.innerHTML;
-    fixedALabel.target = "_blank";
+    var fixedALabel = document.getElementsByClassName("collapsed_link");
+    for (let i = 0; i < fixedALabel.length; i++) {
+        fixedALabel[i].style.display = "inline";
+        fixedALabel[i].href = fixedALabel[i].innerHTML;
+        fixedALabel[i].target = "_blank";
+    }
 })();
