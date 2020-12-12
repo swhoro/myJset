22e4b070f51aa54b7b728cabcaa753cc278f3efb
Aiden <1003077133@qq.com>
2020-12-12 20:20:07 +0800

    add osu.user.js

---
+++ ous.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         osu!下载不含视频文件
+// @version      0.0.1
+// @description  为osu!添加一个不快速下载不需视频文件得按钮
+// @author       Aiden
+// @match        https://osu.ppy.sh/beatmapsets?q=*
+@updateUrl    https://github.com/swhoro/myjset/raw/master/osu.user.js
+// ==/UserScript==
+
+window.onload = function() {
+  "use strict";
+
+  function addAButton(target = document) {
+    let i = target.querySelectorAll("div.beatmapset-panel__icons-box");
+
+    i.forEach(function(item) {
+      let newItem = item.cloneNode(true);
+      newItem.firstChild.title = "下载不包含视频";
+      newItem.firstChild.href = item.href + "?noVideo=1";
+      newItem.firstChild.setAttribute("data-orig-title", "下载不包含视频")
+      newItem.style="margin-left: 4px;"
+      item.parentElement.appendChild(newItem);
+    });
+  }
+
+  addAButton();
+
+  function callback(mutationlists, observer) {
+    mutationlists.forEach(mutationlist => {
+      mutationlist.addedNodes.forEach(node => {
+        addAButton(node.firstChild);
+        addAButton(node.lastChild);
+      });
+    });
+  }
+
+  var observer = new MutationObserver(callback);
+  observer.observe(document.querySelector("div.beatmapsets__items"), {
+    childList: true
+  });
+};
--- readme.md
+++ readme.md
@@ -7,12 +7,16 @@ CSS样式需安装stylus插件
 ## JavaScript脚本文件
 ### [steamDuPanUnblock](https://github.com/swhoro/myjset/raw/master/steamDuPanUnblock.user.js)
 解除steam对百度网盘的封锁
+
 ### [cancelSteamcommunityRelink](https://github.com/swhoro/myjset/raw/master/cancelSteamcommunityRelink.user.js)
 取消steam社区跳转至外部页面
+
 ### [scrollUp](https://github.com/swhoro/myjset/raw/master/scrollUp.user.js)
 在steam相关页面做一个返回顶部按钮
+
 ### [workshopAuto30](https://github.com/swhoro/myjset/raw/master/workshopAuto30.user.js)
 创意工坊订阅页面自动变为每页30个项目
+
 ### [trueLevel](https://github.com/swhoro/myjset/raw/master/trueLevelLazy.user.js)
 在好友和个人资料页面显示真实等级
 
@@ -21,6 +25,9 @@ CSS样式需安装stylus插件
 V2后缀为无需api版本
 
 Lazy为无需api、懒加载版本（默认）
+
+### [osu](https://github.com/swhoro/myjset/raw/master/osu.user.js)
+为osu!搜索页面添加一个下载不需视频文件的按钮
 
 ## CSS样式文件
 ### [baiduReforged](https://github.com/swhoro/myjset/raw/master/baidu.user.css)
